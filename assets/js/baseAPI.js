ee69d593ade7441981722659982828cd174fa04c
zhuweiqi <18755356622@163.com>
2022-09-29 18:48:11 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -12,4 +12,17 @@ $.ajaxPrefilter(function (options) {
       Authorization: localStorage.getItem('token') || ''
     }
   }
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function (res) {
+    // console.log(res)
+    // 在 complete 回调函数中,可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      console.log('ok')
+      // 1.强制清空 token
+      localStorage.removeItem('token')
+      // 2.强制跳转到登录页面
+      location.href = '/login.html'
+    }
+  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -35,17 +35,17 @@ function getUserInfo() {
       renderAvatar(res.data)
     },
     // 不论成功还是失败,最终都会调用 complete 回调函数
-    complete: function (res) {
-      // console.log(res)
-      // 在 complete 回调函数中,可以使用 res.responseJSON 拿到服务器响应回来的数据
-      if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-        console.log('ok')
-        // 1.强制清空 token
-        localStorage.removeItem('token')
-        // 2.强制跳转到登录页面
-        location.href = '/login.html'
-      }
-    }
+    // complete: function (res) {
+    //   // console.log(res)
+    //   // 在 complete 回调函数中,可以使用 res.responseJSON 拿到服务器响应回来的数据
+    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+    //     console.log('ok')
+    //     // 1.强制清空 token
+    //     localStorage.removeItem('token')
+    //     // 2.强制跳转到登录页面
+    //     location.href = '/login.html'
+    //   }
+    // }
   })
 }
 
