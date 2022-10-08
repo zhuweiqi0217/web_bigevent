bbddd190673315e0212fcd2fb0765220eafc6f1b
zhuweiqi <18755356622@163.com>
2022-10-08 16:20:07 +0800

    文章列表和发布文章功能完成

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,5 +1,6 @@
 $(function () {
   let layer = layui.layer
+  let form = layui.form
   initArtCateList()
 
   // 获取文章分类的列表
@@ -9,6 +10,7 @@ $(function () {
       url: '/my/article/cates',
       success(res) {
         // console.log(res)
+        if(res.status !== 0) return layer.msg(res.message)
         let htmlStr = template('tpl-table', res)
         $('tbody').html(htmlStr)
       }
@@ -42,5 +44,66 @@ $(function () {
         layer.close(indexAdd)
       }
     })
+  })
+
+  // 通过代理的形式, 为 btn-edit 按钮绑定点击事件
+  let indexEdit = null
+  $('tbody').on('click', '.btn-edit', function () {
+    // console.log('ok')
+    // 弹出一个修改文字分类信息的层
+    indexEdit = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '修改文章分类',
+      content: $('#dialog-edit').html()
+    })
+
+    let id = $(this).attr('data-id')
+    // 发起请求获取对应分类的数据
+    $.ajax({
+      method: 'GET',
+      url: '/my/article/cates/' + id,
+      success(res) {
+        // console.log(res)
+        if (res.status !== 0) return layer.msg(res.message)
+        // layer.msg(res.message)
+        form.val('form-edit', res.data)
+      }
+    })
+  })
+
+  // 通过代理的形式,为 form-edit 表单绑定 submit 事件
+  $('body').on('submit', '#form-edit', function (e) {
+    e.preventDefault()
+    // console.log(e)
+    $.ajax({
+      method: 'POST',
+      url: '/my/article/updatecate',
+      data: $(this).serialize(),
+      success(res) {
+        if (res.status !== 0) return layer.msg(res.message)
+        layer.msg(res.message)
+        initArtCateList()
+        // 根据索引关闭对应的弹出层
+        layer.close(indexEdit)
+      }
+    })
+  })
+
+  // 为删除按钮绑定点击事件
+  $('tbody').on('click', '.btn-del', function () {
+    const result = confirm('您确认要删除吗? ')
+    let id = $(this).attr('data-id')
+    if (result) {
+      $.ajax({
+        method: 'GET',
+        url: '/my/article/deletecate/' + id,
+        success(res) {
+          if (res.status !== 0) return layer.msg(res.message)
+          layer.msg(res.message)
+          initArtCateList()
+        }
+      })
+    }
   })
 })
